a00365186c673b9d9738e5e80188c90f5044ed90
Oleksii Maksymovych <al.v.maximovich@gmail.com>
2026-01-06 14:03:27 +0200

    Add project

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+function Button({ name, onButtonClick }) {
+  return (
+    <button className="button" onClick={onButtonClick}>
+      {name}
+    </button>
+  );
+}
+export default Button;
+// props = {
+//  name:
+// }
